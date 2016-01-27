56edbc5e5861552d8696932d24b9fdcdb0c3a77a
chetchop <chetchop@users.noreply.github.com>
2016-01-27 14:57:25 -0800

    Update cpu.c

--- 01-queues/cpu.c
+++ 01-queues/cpu.c
@@ -1,5 +1,4 @@
-/*
- * cpu.c
+* cpu.c
  *
  *  Created on: January 21 2016
  *      Author: Melinda Robertson
@@ -7,7 +6,7 @@
  *
  *      Has a loop to simulate a CPU.
  */
- 
+
 #include "pcb.h"
 #include "que.h"
 #include "cpu.h"
@@ -16,6 +15,8 @@
 #include <string.h>
 //holds the PC value when changing PCBs
 unsigned int pseudostack;
+int cntx = 0;
+int cntx2 = 0;
 
 pcb_ptr make_pcb(int pid) {
     /*Create a randomized pcb with the pid.*/
@@ -34,7 +35,7 @@ pcb_ptr make_pcb(int pid) {
 
 cpu_ptr cpu_constructor() {
     /*Construct a CPU that holds a PC and register values.*/
-    cpu_ptr cpu = (cpu_ptr) malloc(sizeof(cpu_type));
+    cpu_ptr cpu = (cpu_ptr) malloc(sizeof(cpu));
     cpu->pc = 0;
     int reg[NUMREGS];
     int i;
@@ -59,38 +60,55 @@ int sch_init_pcb(que_ptr enq, int pid) {
 
 int sch_ready(que_ptr enq, que_ptr rdyq) {
     /*Move all items from the enqueueing queue to the ready queue.*/
-    while(enq->node_count) {
+	printf("\nEnqueing %d pcb's.\n", enq->node_count);
+	while(enq->node_count) {
         pcb_ptr node = q_dequeue(enq);
+        printf("Process has been enqueued --> PCB Contents: %s\n", pcb_toString(node));
         q_enqueue(rdyq, node);
     }
     return 0;
 }
 
-pcb_ptr dispatcher(que_ptr rdyq, pcb_ptr that) {
+pcb_ptr dispatcher(cpu_ptr this, que_ptr rdyq, pcb_ptr that) {
     /*Move the current PCB to the ready queue and return the next one.*/
+	if (cntx >= 3) {
+		printf("\n%s Switching to: %s", pcb_toString(that), pcb_toString(q_peek(rdyq)));
+		cntx = 0;
+	} else if (cntx <= 3) {
+		cntx = cntx + 1;
+	}
     that->pc = this->pc;
     memcpy(that->reg_file, this->reg_file, NUMREGS);
     q_enqueue(rdyq, that);
-    
+
     pcb_ptr current = q_dequeue(rdyq);
     memcpy(this->reg_file, current->reg_file, NUMREGS);
     current->state = running;
     pseudostack = current->pc;
+
+    if (cntx2 >= 3) {
+		printf("\n%s Switching to: %s\n", pcb_toString(that), pcb_toString(q_peek(rdyq)));
+		printf("%s\n", q_toString(rdyq));
+		cntx2 = 0;
+	} else if (cntx2 <= 3) {
+		cntx2 = cntx2 + 1;
+	}
+
     return current;
 }
 
-pcb_ptr scheduler(que_ptr rdyq, pcb_ptr that, enum state_type inter) {
+pcb_ptr scheduler(cpu_ptr this, que_ptr rdyq, pcb_ptr that, enum state_type inter) {
     switch(inter) {
         case interrupted:
-        that->state ready;
-        return dispatcher(rdyq, that);
+        that->state = ready;
+        return dispatcher(this, rdyq, that);
         break;
         case ready:
         //something far in the future
         break;
     }
     //something went wrong
-    return null;
+    return NULL;
 }
 
 pcb_ptr isr(cpu_ptr this, que_ptr rdyq, pcb_ptr current) {
@@ -99,7 +117,7 @@ pcb_ptr isr(cpu_ptr this, que_ptr rdyq, pcb_ptr current) {
     //set to interrupted
     current->state = interrupted;
     //call scheduler
-    pcb_ptr newcurrent = scheduler(rdyq, current, current->state);
+    pcb_ptr newcurrent = scheduler(this, rdyq, current, current->state);
     //pseudo push to stack
     this->pc = pseudostack;
     return newcurrent;
@@ -121,6 +139,7 @@ int cpu_loop(cpu_ptr this) {
      pid = pid + 1;
      pseudostack = current->pc;
      this->pc = pseudostack;
+
      while(run) {
          if(pid < 30) {
             //create 0-5 processes; add to enqueueing queue
@@ -132,8 +151,14 @@ int cpu_loop(cpu_ptr this) {
          addto = ((rand() % 1000) + 3000);
          this->pc = this->pc + addto;
          //call isr -> scheduler -> dispatcher
-         current = isr(this, rdyq, current);         
+         current = isr(this, rdyq, current);
          //timer interrupt
          run = run - 1;
      }
+
  }
+int main() {
+	 cpu_ptr cpu1 = cpu_constructor();
+	 cpu_loop(cpu1);
+	 return 0;
+}
